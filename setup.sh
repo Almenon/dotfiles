2558e787f3b1b4048d6c740c03fa799982c50375
Almenon <almenon214@gmail.com>
2025-07-28 21:48:34 -0700

    add setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#$/bin/bash
+set -Eeuo pipefail
+
+if [ "$UNAME" == "Linux" ]; then
+    # If available, use LSB to identify distribution
+    if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
+        export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+    # Otherwise, use release info file
+    else
+        export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
+    fi
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install pipx
+    pipx ensurepath
+    sudo pipx ensurepath --global
+fi
+elif ["$DISTRO" == "Ubuntu" ]; then
+    apt install net-tools
+    apt install dnsutils
+fi
