e7171e4e2dad3657a8342de36fa8788c29f870fd
Almenon <almenon214@gmail.com>
2025-07-30 21:55:08 -0700

    add explanation

--- eyes.sh
+++ eyes.sh
@@ -3,6 +3,7 @@ set -Eeou pipefail
 # This script reminds me to take a quick eye break every 20 minutes
 # I get a headache if I don't do this :(
 # https://www.mayoclinic.org/diseases-conditions/eyestrain/diagnosis-treatment/drc-20372403
+# Note for this to work you need to execute the display statement manually in apple script first
 
 while :
 do
